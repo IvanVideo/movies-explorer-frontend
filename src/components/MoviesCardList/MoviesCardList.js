739740ca8480271b575a85b2eb9ead43340c9b5d
buka69 <ramazanov.iv@gmail.com>
2021-07-25 15:42:42 +0300

    fix: поправил отображение неактивного лайка и отображение крестика при мобильном разрешение в разделе сохр фильмах

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SearchForm from '../SearchForm/SearchForm';
 import React from 'react';
+import film from '../../images/pic.svg';
 
 const MoviesCardList = () => {
     return (
@@ -14,11 +15,39 @@ const MoviesCardList = () => {
                 <div className='moviesCardList-conteiner'>
                     <MoviesCard />
                     <MoviesCard />
+                    <section className='moviesCard'>
+                        <div className='moviesCard-content'>
+                            <h2 className='moviesCard-title'>33 слова о дизайне</h2>
+                            <p className='moviesCard-time'>1ч 42м</p>
+                            <button className='moviesCard-like white'></button>
+                        </div>
+                        <img className='moviesCard-img' alt='picture' src={film} />
+                    </section>
+                    <section className='moviesCard'>
+                        <div className='moviesCard-content'>
+                            <h2 className='moviesCard-title'>33 слова о дизайне</h2>
+                            <p className='moviesCard-time'>1ч 42м</p>
+                            <button className='moviesCard-like'></button>
+                        </div>
+                        <img className='moviesCard-img' alt='picture' src={film} />
+                    </section>
                     <MoviesCard />
-                    <MoviesCard />
-                    <MoviesCard />
-                    <MoviesCard />
-                    <MoviesCard />
+                    <section className='moviesCard'>
+                        <div className='moviesCard-content'>
+                            <h2 className='moviesCard-title'>33 слова о дизайне</h2>
+                            <p className='moviesCard-time'>1ч 42м</p>
+                            <button className='moviesCard-like white'></button>
+                        </div>
+                        <img className='moviesCard-img' alt='picture' src={film} />
+                    </section>
+                    <section className='moviesCard'>
+                        <div className='moviesCard-content'>
+                            <h2 className='moviesCard-title'>33 слова о дизайне</h2>
+                            <p className='moviesCard-time'>1ч 42м</p>
+                            <button className='moviesCard-like white'></button>
+                        </div>
+                        <img className='moviesCard-img' alt='picture' src={film} />
+                    </section>
                 </div>
                 <button className='moviesCardList-button'>Ещё</button>
             </div>
