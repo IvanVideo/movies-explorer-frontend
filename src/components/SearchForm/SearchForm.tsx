f4c97bc2a336ded6685d21a4fea21a13140b85da
IvanVideo <ramazanov.iv@gmail.com>
2021-12-04 17:33:55 +0300

    refactor: переписал компонент поиска фильмов

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,4 +1,7 @@
 import "./SearchForm.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators, State } from '../../store';
 import React, { useEffect } from "react";
 import checkbox from "../../images/check.svg";
 import checkbox2 from "../../images/black.svg";
@@ -9,29 +12,33 @@ interface SearchFormProps {
   shortFilmsStatus: Function,
 }
 
-const SearchForm = ({enterValue, enterValueSaved, shortFilmsStatus }: SearchFormProps) => {
-  const [inputValue, setInputValue] = React.useState("");
-  const [checkboxState, setCheckboxState] = React.useState(false);
-  const [status, setStatus] = React.useState(false);
+const SearchForm = ({ enterValue, enterValueSaved, shortFilmsStatus }: SearchFormProps) => {
+  const dispatch = useDispatch();
+  const { getInputValue } = bindActionCreators(actionCreators, dispatch);
+  const { changeChackboxValue } = bindActionCreators(actionCreators, dispatch);
+  const { changeInputErrorValue } = bindActionCreators(actionCreators, dispatch);
+  const input = useSelector((state: State) => state.inputValue);
+  const chackboxValue = useSelector((state: State) => state.chackbox);
+  const inputError = useSelector((state: State) => state.inputError);
 
-  const handleChangeInputValue = (e: React.FormEvent) => {
-    // setInputValue(e.target.value);
+  const handleChangeInputValue = (e: any) => {
+    getInputValue(e.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-    if (inputValue == "") {
-      setStatus(true);
+    if (input == "") {
+      changeInputErrorValue(true);
     } else {
-      setStatus(false);
-      enterValue(inputValue);
-      enterValueSaved(inputValue);
+      changeInputErrorValue(false);
+      enterValue(input);
+      enterValueSaved(input);
     }
   };
 
   const handleShortFilmsClick = () => {
-    setCheckboxState(!checkboxState);
-    shortFilmsStatus(checkboxState);
+    changeChackboxValue(!chackboxValue);
+    shortFilmsStatus(chackboxValue);
   };
 
   return (
@@ -53,13 +60,13 @@ const SearchForm = ({enterValue, enterValueSaved, shortFilmsStatus }: SearchForm
             />
             <label htmlFor="id1">
               <img
-                src={checkboxState ? checkbox : checkbox2}
+                src={chackboxValue ? checkbox : checkbox2}
                 alt="radiobutton"
                 className="searchForm-radio"
               />
             </label>
             <p className="searchForm__shortfilms">Короткометражки</p>
-            <span className={status ? "error" : "error__unvisible"}>
+            <span className={inputError ? "error" : "error__unvisible"}>
               Нужно ввести ключевое слово
             </span>
           </div>
